da507d2319ecdabcad98a661767d56f5a52ddcc4
ruhaimmohammed <ruhaimmohammedk@gmail.com>
2023-12-13 00:16:42 +0530

    BST travarsel and height

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import { Stacks } from './stacks.js'
 import { Sets } from './sets.js'
 import { Queue } from './queue.js'
 import { BinarySearchTree } from './binarySearchTree.js'
+import BstTravarselHeight from './bstTravarselHeight.js';
 
 // Stacks();
 // Sets();
 // Queue();
-BinarySearchTree();
+// BinarySearchTree();
+BstTravarselHeight();
